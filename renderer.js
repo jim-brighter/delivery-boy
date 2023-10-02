578a2780294abe315faedee08c56209268b5b570
Jim Brighter <jbrighter92@gmail.com>
2023-10-01 21:42:09 -0400

    headers editor

--- renderer.js
+++ renderer.js
@@ -20,10 +20,25 @@ const sendRequest = async () => {
     const method = document.getElementById('method').value;
     const url = document.getElementById('url').value;
 
+    const headerTable = document.getElementById('headers-table');
+    const headerTableBody = headerTable.getElementsByTagName('tbody')[0];
+    const headerTableRows = headerTableBody.getElementsByTagName('tr');
+
+    const headers = {};
+
+    for (let headerRow of headerTableRows) {
+        const cells = headerRow.getElementsByTagName('td');
+        const headerName = cells[0].getElementsByTagName('input')[0].value;
+        const headerVal = cells[1].getElementsByTagName('input')[0].value;
+
+        headers[headerName] = headerVal;
+    }
+
     const responseArea = document.getElementById('response-body');
 
     const response = await fetch(url, {
-        method
+        method,
+        headers
     });
 
     const data = await response.json();
@@ -31,4 +46,36 @@ const sendRequest = async () => {
     responseArea.innerText = JSON.stringify(data, null, 2);
 }
 
-document.getElementById('send').onclick = sendRequest;
+const changeDetails = (event) => {
+    document.querySelectorAll('.request-details-editor').forEach((element) => {
+        element.hidden = element.id !== `${event.target.id}-editor`;
+    });
+}
+
+const newHeader = () => {
+    const headerTable = document.getElementById('headers-table');
+    const headerTableBody = headerTable.getElementsByTagName('tbody')[0];
+
+    const row = document.createElement('tr');
+    const headerCol = document.createElement('td');
+    const headerVal = document.createElement('td');
+
+    const headerColInput = document.createElement('input');
+    headerColInput.classList.add('header-input');
+    headerColInput.type = 'text';
+    headerColInput.autocomplete = 'off';
+    headerColInput.placeholder = 'Content-Type';
+
+    const headerValInput = document.createElement('input');
+    headerValInput.classList.add('header-input');
+    headerValInput.type = 'text';
+    headerValInput.autocomplete = 'off';
+    headerValInput.placeholder = 'application/json';
+
+    headerCol.appendChild(headerColInput);
+    headerVal.appendChild(headerValInput);
+    row.appendChild(headerCol);
+    row.appendChild(headerVal);
+
+    headerTableBody.appendChild(row);
+}
